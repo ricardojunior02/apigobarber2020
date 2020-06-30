c75938eba35d36a5be8ae169c8e034dd731dc6f1
ricardojunior02 <ricardojunior199502@gmail.com>
2020-06-30 11:56:21 -0300

    Repository de users

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,67 @@
+import { getRepository, Repository, Not } from 'typeorm';
+
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+class UserRepository implements IUserRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.ormRepository.findOne(id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const userEmail = await this.ormRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return userEmail;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users: User[];
+
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
+  }
+
+  public async create({
+    email,
+    password,
+    name,
+  }: ICreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create({ email, password, name });
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    return this.ormRepository.save(user);
+  }
+}
+
+export default UserRepository;
