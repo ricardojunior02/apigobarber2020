9d630476d28b86a7bb63bec5234d8a5c8f75c002
ricardojunior02 <ricardojunior199502@gmail.com>
2020-06-30 12:01:58 -0300

    Rate limiter

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import redis from 'redis';
+
+import AppError from '@shared/error/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 40,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Tpp maany requests', 429);
+  }
+}
