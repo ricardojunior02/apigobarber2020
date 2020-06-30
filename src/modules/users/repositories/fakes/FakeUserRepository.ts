d76c6601a03b8d0b64b033d8aeb151a377e2fd36
ricardojunior02 <ricardojunior199502@gmail.com>
2020-06-30 11:59:12 -0300

    Classes fake para testes

---
+++ src/modules/users/repositories/fakes/FakeUserRepository.ts
@@ -0,0 +1,59 @@
+import { uuid } from 'uuidv4';
+
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+class UserRepository implements IUserRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
+  }
+
+  public async create({
+    email,
+    password,
+    name,
+  }: ICreateUserDTO): Promise<User> {
+    const createUser = new User();
+
+    Object.assign(createUser, { id: uuid(), email, password, name });
+
+    this.users.push(createUser);
+
+    return createUser;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UserRepository;
---
+++ src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+import IUserToken from '@modules/users/repositories/IUserTokensRepository';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+export default class FakeUserTokenRepository implements IUserToken {
+  private usersTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.usersTokens.find(
+      findToken => findToken.token === token
+    );
+
+    return userToken;
+  }
+}
