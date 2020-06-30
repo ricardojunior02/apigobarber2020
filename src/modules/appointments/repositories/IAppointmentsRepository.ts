58a69b048529833999e548ce254aea03d1dd1869
ricardojunior02 <ricardojunior199502@gmail.com>
2020-06-30 11:50:50 -0300

    Interface de appointments

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,15 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayhFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO
+  ): Promise<Appointment[]>;
+  findAllInDayProvider(
+    data: IFindAllInDayhFromProviderDTO
+  ): Promise<Appointment[]>;
+}
